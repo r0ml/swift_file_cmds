cb8c7fa20975059f24778fd40dd35575bf0e8869
Robert Lefkowitz <r0ml@mac.com>
2026-01-01 19:44:05 -0500

    touch

--- Sources/touch/touch.swift
+++ Sources/touch/touch.swift
@@ -32,400 +32,391 @@
  * SUCH DAMAGE.
  */
 
-#include <sys/cdefs.h>
+import CMigration
+import Darwin
 
-__FBSDID("$FreeBSD$");
+@main struct touch : ShellCommand {
 
-#ifndef lint
-__used static const char copyright[] =
-"@(#) Copyright (c) 1993\n\
-	The Regents of the University of California.  All rights reserved.\n";
-#endif
+  struct CommandOptions {
+    var ts = (timespec(), timespec())
+    var atflag = false
+    var Aflag = 0
+    var aflag = false
+    var cflag = false
+    var mflag = false
+    var timese = false
+    var args : [String] = []
+    var timeset = false
+  }
 
-#ifndef lint
-__used static const char sccsid[] = "@(#)touch.c	8.1 (Berkeley) 6/6/93";
-#endif
+  // ch, fd, len, rval, timeset;
+  //	char *p;
+  //	char *myname;
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/time.h>
+  //	myname = basename(argv[0]);
 
-#include <ctype.h>
-#include <err.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <libgen.h>
-#ifdef __APPLE__
-#include <stdbool.h>
-#endif
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
+  var options : CommandOptions!
 
-#ifdef __APPLE__
-#define	st_atim	st_atimespec
-#define	st_mtim	st_mtimespec
-#endif
+  func parseOptions() async throws(CmdErr) -> CommandOptions {
+    var options = CommandOptions()
 
-static void	stime_arg1(const char *, struct timespec *);
-static void	stime_arg2(const char *, int, struct timespec *);
-static void	stime_darg(const char *, struct timespec *);
-static void	stime_file(const char *, struct timespec *);
-static int	timeoffset(const char *);
-static void	usage(const char *);
+    options.ts.0.tv_sec = 0
+    options.ts.1.tv_sec = 0
+    options.ts.0.tv_nsec = Int(UTIME_NOW)
+    options.ts.1.tv_nsec = Int(UTIME_NOW)
 
-int
-main(int argc, char *argv[])
-{
-	struct stat sb;
-	struct timespec ts[2];
-	int atflag;
-	int Aflag, aflag, cflag, mflag, ch, fd, len, rval, timeset;
-	char *p;
-	char *myname;
+    let go = BSDGetopt("A:acd:fhmr:t:")
+    while let (k,v) = try go.getopt() {
+      switch k {
+        case "A":
+          options.Aflag = Int(timeoffset(v))
+        case "a":
+          options.aflag = true
+        case "c":
+          options.cflag = true
+        case "d":
+          options.timeset = true
+          options.ts = stime_darg(v);
+        case "f":
+          /* No-op for compatibility. */
+          break;
+        case "h":
+          options.cflag = true
+          options.atflag = AT_SYMLINK_NOFOLLOW != 0
+        case "m":
+          options.mflag = true
+        case "r":
+          options.timeset = true
+          options.ts = stime_file(v)
+        case "t":
+          options.timeset = true
+          options.ts = stime_arg1(v)
+        default:
+          throw CmdErr(1)
+      }
+    }
+    options.args = go.remaining
 
-	myname = basename(argv[0]);
-	Aflag = aflag = cflag = mflag = timeset = 0;
-	atflag = 0;
-	ts[0].tv_sec = ts[1].tv_sec = 0;
-	ts[0].tv_nsec = ts[1].tv_nsec = UTIME_NOW;
+    if !options.aflag && !options.mflag {
+      options.aflag = true
+      options.mflag = true
+    }
 
-	while ((ch = getopt(argc, argv, "A:acd:fhmr:t:")) != -1)
-		switch(ch) {
-		case 'A':
-			Aflag = timeoffset(optarg);
-			break;
-		case 'a':
-			aflag = 1;
-			break;
-		case 'c':
-			cflag = 1;
-			break;
-		case 'd':
-			timeset = 1;
-			stime_darg(optarg, ts);
-			break;
-		case 'f':
-			/* No-op for compatibility. */
-			break;
-		case 'h':
-			cflag = 1;
-			atflag = AT_SYMLINK_NOFOLLOW;
-			break;
-		case 'm':
-			mflag = 1;
-			break;
-		case 'r':
-			timeset = 1;
-			stime_file(optarg, ts);
-			break;
-		case 't':
-			timeset = 1;
-			stime_arg1(optarg, ts);
-			break;
-		default:
-			usage(myname);
-		}
-	argc -= optind;
-	argv += optind;
+    if options.timeset {
+      if options.Aflag != 0 {
+        /*
+         * We're setting the time to an offset from a specified
+         * time.  God knows why, but it means that we can set
+         * that time once and for all here.
+         */
+        if options.aflag {
+          options.ts.0.tv_sec += options.Aflag
+        }
+        if options.mflag {
+          options.ts.1.tv_sec += options.Aflag
+        }
+        options.Aflag = 0		/* done our job */
+      }
+    } else {
+      /*
+       * If no -r or -t flag, at least two operands, the first of
+       * which is an 8 or 10 digit number, use the obsolete time
+       * specification, otherwise use the current time.
+       */
+      if options.args.count > 1 {
+        let p = options.args[0].prefix { $0.isNumber }
+        if p.count == 8 || p.count == 10 {
+          options.timeset = true
+          options.ts = stime_arg2(String(p), p.count == 10)
+          options.args.removeFirst()
+        }
+      }
+      /* Both times default to the same. */
+      options.ts.1 = options.ts.0
+    }
 
-	if (aflag == 0 && mflag == 0)
-		aflag = mflag = 1;
+    if !options.aflag {
+      options.ts.0.tv_nsec = Int(UTIME_OMIT)
+    }
+    if !options.mflag {
+      options.ts.1.tv_nsec = Int(UTIME_OMIT)
+    }
 
-	if (timeset) {
-		if (Aflag) {
-			/*
-			 * We're setting the time to an offset from a specified
-			 * time.  God knows why, but it means that we can set
-			 * that time once and for all here.
-			 */
-			if (aflag)
-				ts[0].tv_sec += Aflag;
-			if (mflag)
-				ts[1].tv_sec += Aflag;
-			Aflag = 0;		/* done our job */
-		}
-	} else {
-		/*
-		 * If no -r or -t flag, at least two operands, the first of
-		 * which is an 8 or 10 digit number, use the obsolete time
-		 * specification, otherwise use the current time.
-		 */
-		if (argc > 1) {
-			strtol(argv[0], &p, 10);
-			len = p - argv[0];
-			if (*p == '\0' && (len == 8 || len == 10)) {
-				timeset = 1;
-				stime_arg2(*argv++, len == 10, ts);
-			}
-		}
-		/* Both times default to the same. */
-		ts[1] = ts[0];
-	}
+    if options.args.isEmpty {
+      throw CmdErr(1)
+    }
 
-	if (!aflag)
-		ts[0].tv_nsec = UTIME_OMIT;
-	if (!mflag)
-		ts[1].tv_nsec = UTIME_OMIT;
+    if options.Aflag != 0 {
+      options.cflag = true
+    }
+    return options
+  }
 
-	if (*argv == NULL)
-		usage(myname);
+  var DEFFILEMODE = (S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH)
 
-	if (Aflag)
-		cflag = 1;
+  func runCommand() async throws(CmdErr) {
+    var rval : Int32 = 0
+    var sb = stat()
+    for argv in options.args {
+      /* See if the file exists. */
+      if (fstatat(AT_FDCWD, argv, &sb, options.atflag ? 1 : 0) != 0) {
+        if (errno != ENOENT) {
+          rval = 1;
+          warn(argv)
+          continue;
+        }
+        if !options.cflag {
 
-	for (rval = 0; *argv; ++argv) {
-		/* See if the file exists. */
-		if (fstatat(AT_FDCWD, *argv, &sb, atflag) != 0) {
-			if (errno != ENOENT) {
-				rval = 1;
-				warn("%s", *argv);
-				continue;
-			}
-			if (!cflag) {
-#ifdef __APPLE__
-				bool cfail;
-#endif
-				/* Create the file. */
-				fd = open(*argv,
-				    O_WRONLY | O_CREAT, DEFFILEMODE);
-#ifdef __APPLE__
-				/*
-				 * See rdar://problem/69195889 -- don't leak
-				 * fd on failure.
-				 */
-				cfail = fd == -1 || fstat(fd, &sb) != 0;
-				if (fd != -1)
-					(void)close(fd);
-				if (cfail) {
-#else
-				if (fd == -1 || fstat(fd, &sb) || close(fd)) {
-#endif
-					rval = 1;
-					warn("%s", *argv);
-					continue;
-				}
+          /* Create the file. */
+          let fd = open(argv, O_WRONLY | O_CREAT, DEFFILEMODE);
 
-				/* If using the current time, we're done. */
-				if (!timeset)
-					continue;
-			} else
-				continue;
-		}
+          /*
+           * See rdar://problem/69195889 -- don't leak
+           * fd on failure.
+           */
+          let cfail = fd == -1 || fstat(fd, &sb) != 0;
+          if fd != -1 {
+            close(fd)
+          }
+          if cfail {
+            rval = 1;
+            warn(argv);
+            continue;
+          }
 
-		/*
-		 * We're adjusting the times based on the file times, not a
-		 * specified time (that gets handled above).
-		 */
-		if (Aflag) {
-			if (aflag) {
-				ts[0] = sb.st_atim;
-				ts[0].tv_sec += Aflag;
-			}
-			if (mflag) {
-				ts[1] = sb.st_mtim;
-				ts[1].tv_sec += Aflag;
-			}
-		}
+          /* If using the current time, we're done. */
+          if (!options.timeset) {
+            continue;
+          }
+        } else {
+          continue
+        }
+      }
 
-		if (!utimensat(AT_FDCWD, *argv, ts, atflag))
-			continue;
+      var ts = options.ts
 
-		rval = 1;
-		warn("%s", *argv);
-	}
-	exit(rval);
-}
+      /*
+       * We're adjusting the times based on the file times, not a
+       * specified time (that gets handled above).
+       */
+      if options.Aflag != 0 {
+        if options.aflag {
+          ts.0 = sb.st_atim
+          ts.0.tv_sec += options.Aflag
+        }
+        if options.mflag {
+          ts.1 = sb.st_mtim
+          ts.1.tv_sec += options.Aflag
+        }
+      }
 
-#define	ATOI2(ar)	((ar)[0] - '0') * 10 + ((ar)[1] - '0'); (ar) += 2;
+      if utimensat(AT_FDCWD, argv, &ts.0, options.atflag ? 1 : 0) == 0 {
+        continue;
+      }
 
-static void
-stime_arg1(const char *arg, struct timespec *tvp)
-{
-	time_t now;
-	struct tm *t;
-	int yearset;
-	char *p;
+      rval = 1;
+      warn(argv)
+    }
+    exit(rval);
+  }
 
-	now = time(NULL);
-	if ((t = localtime(&now)) == NULL)
-		err(1, "localtime");
-					/* [[CC]YY]MMDDhhmm[.SS] */
-	if ((p = strchr(arg, '.')) == NULL)
-		t->tm_sec = 0;		/* Seconds defaults to 0. */
-	else {
-		if (strlen(p + 1) != 2)
-			goto terr;
-		*p++ = '\0';
-		t->tm_sec = ATOI2(p);
-	}
+  func ATOI2(_ ar : inout String) -> Int32 {
+    let k = Int32(ar.prefix(2))
+    ar.removeFirst(2)
+    return k ?? 0
+  }
 
-	yearset = 0;
-	switch(strlen(arg)) {
-	case 12:			/* CCYYMMDDhhmm */
-		t->tm_year = ATOI2(arg);
-		t->tm_year *= 100;
-		yearset = 1;
-		/* FALLTHROUGH */
-	case 10:			/* YYMMDDhhmm */
-		if (yearset) {
-			yearset = ATOI2(arg);
-			t->tm_year += yearset;
-		} else {
-			yearset = ATOI2(arg);
-			if (yearset < 69)
-				t->tm_year = yearset + 2000;
-			else
-				t->tm_year = yearset + 1900;
-		}
-		t->tm_year -= 1900;	/* Convert to UNIX time. */
-		/* FALLTHROUGH */
-	case 8:				/* MMDDhhmm */
-		t->tm_mon = ATOI2(arg);
-		--t->tm_mon;		/* Convert from 01-12 to 00-11 */
-		t->tm_mday = ATOI2(arg);
-		t->tm_hour = ATOI2(arg);
-		t->tm_min = ATOI2(arg);
-		break;
-	default:
-		goto terr;
-	}
+  //    #define	ATOI2(ar)	((ar)[0] - '0') * 10 + ((ar)[1] - '0'); (ar) += 2;
 
-	t->tm_isdst = -1;		/* Figure out DST. */
-	tvp[0].tv_sec = tvp[1].tv_sec = mktime(t);
-	if (tvp[0].tv_sec == -1)
-		goto terr;
+  func stime_arg1(_ argx : String) -> (timespec, timespec) {
+//    time_t now;
+//    struct tm *t;
+//    char *p;
+    var yearset : Int32 = 0
+    var arg = argx
+    var now = time(nil)
+    var tvp = (timespec(), timespec())
+    var tt = localtime(&now)
+    if tt == nil {
+      err(1, "localtime")
+    }
 
-	tvp[0].tv_nsec = tvp[1].tv_nsec = 0;
-	return;
+    var t = tt!.pointee
 
-terr:
-	errx(1, "out of range or illegal time specification: [[CC]YY]MMDDhhmm[.SS]");
-}
+    /* [[CC]YY]MMDDhhmm[.SS] */
 
-static void
-stime_arg2(const char *arg, int year, struct timespec *tvp)
-{
-	time_t now;
-	struct tm *t;
+    t.tm_sec = 0
+    if let p = arg.firstIndex(of: ".") {
+      var pp = String(arg[arg.index(after: p)...])
+      if pp.count == 2 {
+        t.tm_sec = ATOI2(&pp)
+        arg = String(arg[..<p])
+      } else {
+        errx(1, "out of range or illegal time specification: [[CC]YY]MMDDhhmm[.SS]");
+      }
+    }
+    yearset = 0
+    switch(arg.count) {
+      case 12:			/* CCYYMMDDhhmm */
+        t.tm_year = ATOI2(&arg)
+        t.tm_year *= 100
+        yearset = 1
+        fallthrough
+      case 10:			/* YYMMDDhhmm */
+        if yearset != 0 {
+          yearset = ATOI2(&arg)
+          t.tm_year += yearset
+        } else {
+          yearset = ATOI2(&arg)
+          if yearset < 69 {
+            t.tm_year = yearset + 2000
+          }
+          else {
+            t.tm_year = yearset + 1900
+          }
+        }
+        t.tm_year -= 1900	/* Convert to UNIX time. */
+        fallthrough
+      case 8:				/* MMDDhhmm */
+        t.tm_mon = ATOI2(&arg)
+        t.tm_mon -= 1		/* Convert from 01-12 to 00-11 */
+        t.tm_mday = ATOI2(&arg);
+        t.tm_hour = ATOI2(&arg);
+        t.tm_min = ATOI2(&arg);
+      default:
+        errx(1, "out of range or illegal time specification: [[CC]YY]MMDDhhmm[.SS]");
+    }
 
-	now = time(NULL);
-	if ((t = localtime(&now)) == NULL)
-		err(1, "localtime");
+    t.tm_isdst = -1		/* Figure out DST. */
+    tvp.0.tv_sec = mktime(&t)
+    tvp.1.tv_sec = tvp.0.tv_sec
+    /// Ha Ha Ha -- the tests use a time which is -1 -- registers as an error
+    if tvp.0.tv_sec != -1 {
+      tvp.0.tv_nsec = 0
+      tvp.1.tv_nsec = 0
+      return tvp
+    }
+    errx(1, "out of range or illegal time specification: [[CC]YY]MMDDhhmm[.SS]")
+    return tvp
+  }
 
-	t->tm_mon = ATOI2(arg);		/* MMDDhhmm[yy] */
-	--t->tm_mon;			/* Convert from 01-12 to 00-11 */
-	t->tm_mday = ATOI2(arg);
-	t->tm_hour = ATOI2(arg);
-	t->tm_min = ATOI2(arg);
-	if (year) {
-		t->tm_year = ATOI2(arg);
-		if (t->tm_year < 39)	/* support 2000-2038 not 1902-1969 */
-			t->tm_year += 100;
-	}
+  func stime_arg2(_ argx : String, _ year : Bool) -> (timespec, timespec) {
+//    time_t now;
+//    struct tm *t;
+    var arg = argx
+    var now = time(nil)
+    var tt = localtime(&now)
+    var tvp = (timespec(), timespec() )
 
-	t->tm_isdst = -1;		/* Figure out DST. */
-	tvp[0].tv_sec = tvp[1].tv_sec = mktime(t);
-	if (tvp[0].tv_sec == -1)
-		errx(1,
-	"out of range or illegal time specification: MMDDhhmm[yy]");
+    if tt == nil {
+      err(1, "localtime");
+    }
+    var t = tt!.pointee
+    t.tm_mon = ATOI2(&arg)		/* MMDDhhmm[yy] */
+    t.tm_mon -= 1			/* Convert from 01-12 to 00-11 */
+    t.tm_mday = ATOI2(&arg)
+    t.tm_hour = ATOI2(&arg)
+    t.tm_min = ATOI2(&arg)
+    if year {
+      t.tm_year = ATOI2(&arg)
+      if t.tm_year < 39	{ /* support 2000-2038 not 1902-1969 */
+        t.tm_year += 100
+      }
+    }
 
-	tvp[0].tv_nsec = tvp[1].tv_nsec = 0;
-}
+    t.tm_isdst = -1		/* Figure out DST. */
+    tvp.0.tv_sec = mktime(&t)
+    tvp.1.tv_sec = tvp.0.tv_sec
+    if tvp.0.tv_sec == -1 {
+      errx(1, "out of range or illegal time specification: MMDDhhmm[yy]")
+    }
 
-static void
-stime_darg(const char *arg, struct timespec *tvp)
-{
-	struct tm t = { .tm_sec = 0 };
-	const char *fmt, *colon;
-	char *p;
-	int val, isutc = 0;
+    tvp.0.tv_nsec = 0
+    tvp.1.tv_nsec = 0
+    return tvp
+  }
 
-	tvp[0].tv_nsec = 0;
-	t.tm_isdst = -1;
-	colon = strchr(arg, ':');
-	if (colon == NULL || strchr(colon + 1, ':') == NULL)
-		goto bad;
-	fmt = strchr(arg, 'T') != NULL ? "%Y-%m-%dT%H:%M:%S" :
-	    "%Y-%m-%d %H:%M:%S";
-	p = strptime(arg, fmt, &t);
-	if (p == NULL)
-		goto bad;
-	/* POSIX: must have at least one digit after dot */
-	if ((*p == '.' || *p == ',') && isdigit((unsigned char)p[1])) {
-		p++;
-		val = 100000000;
-		while (isdigit((unsigned char)*p)) {
-			tvp[0].tv_nsec += val * (*p - '0');
-			p++;
-			val /= 10;
-		}
-	}
-	if (*p == 'Z') {
-		isutc = 1;
-		p++;
-	}
-	if (*p != '\0')
-		goto bad;
+  func stime_darg(_ arg : String) -> (timespec, timespec) {
+    var t = tm()
+    t.tm_sec = 0
 
-	tvp[0].tv_sec = isutc ? timegm(&t) : mktime(&t);
+//    const char *fmt, *colon;
+//    char *p;
+//    int val, isutc = 0;
 
-	tvp[1] = tvp[0];
-	return;
+    var isutc = false
+    var tvp = (timespec(), timespec() )
+    tvp.0.tv_nsec = 0
+    t.tm_isdst = -1
+    let colon = arg.firstIndex(of: ":")
+    if !(colon == nil || arg[arg.index(after: colon!)...].firstIndex(of: ":") == nil) {
+      let fmt = arg.firstIndex(of: "T") != nil ? "%Y-%m-%dT%H:%M:%S" :
+      "%Y-%m-%d %H:%M:%S";
+      let p = strptime(arg, fmt, &t)
+      if let p {
+        /* POSIX: must have at least one digit after dot */
+        var pp = String(cString: p)
+        if pp.hasPrefix(".") || pp.hasPrefix(",") {
+          pp.removeFirst()
+          let zz = pp.prefix { $0.isNumber }
+          if !zz.isEmpty, let kk = Double("0."+zz) {
+              tvp.0.tv_nsec = Int(kk * 1_000_000_000)
+          } else {
+            errx(1, "out of range or illegal time specification: YYYY-MM-DDThh:mm:SS[.frac][tz]");
+          }
+        }
+        if arg.hasSuffix("Z") {
+          isutc = true
+        }
+        if pp.isEmpty {
+          tvp.0.tv_sec = isutc ? timegm(&t) : mktime(&t);
+          tvp.1 = tvp.0
+          return tvp
+        }
+      }
+    }
+//  bad:
+    errx(1, "out of range or illegal time specification: YYYY-MM-DDThh:mm:SS[.frac][tz]");
+    return tvp
+  }
 
-bad:
-	errx(1, "out of range or illegal time specification: YYYY-MM-DDThh:mm:SS[.frac][tz]");
-}
+  /* Calculate a time offset in seconds, given an arg of the format [-]HHMMSS. */
+  func timeoffset(_ argx : String) -> Int32 {
+    var arg = argx
+    var offset : Int32 = 0
+    let isneg = arg.hasPrefix("-")
+    if isneg {
+      arg.removeFirst()
+    }
+    switch arg.count {
 
-/* Calculate a time offset in seconds, given an arg of the format [-]HHMMSS. */
-int
-timeoffset(const char *arg)
-{
-	int offset;
-	int isneg;
+      case 6:					/* HHMMSS */
+        offset = ATOI2(&arg)
+        fallthrough
+      case 4:					/* MMSS */
+        offset = offset * 60 + ATOI2(&arg)
+        fallthrough
+      case 2:					/* SS */
+        offset = offset * 60 + ATOI2(&arg)
 
-	offset = 0;
-	isneg = *arg == '-';
-	if (isneg)
-		arg++;
-	switch (strlen(arg)) {
-	default:				/* invalid */
-		errx(1, "Invalid offset spec, must be [-][[HH]MM]SS");
+      default:        /* invalid */
+        errx(1, "Invalid offset spec, must be [-][[HH]MM]SS");
+    }
+    return isneg ? -offset : offset
+  }
 
-	case 6:					/* HHMMSS */
-		offset = ATOI2(arg);
-		/* FALLTHROUGH */
-	case 4:					/* MMSS */
-		offset = offset * 60 + ATOI2(arg);
-		/* FALLTHROUGH */
-	case 2:					/* SS */
-		offset = offset * 60 + ATOI2(arg);
-	}
-	if (isneg)
-		return (-offset);
-	else
-		return (offset);
-}
+  func stime_file(_ fname : String) -> (timespec, timespec) {
+    var sb = stat()
+    if stat(fname, &sb) < 0 {
+      err(1, fname)
+    }
+    return (sb.st_atim, sb.st_mtim)
+  }
 
-static void
-stime_file(const char *fname, struct timespec *tsp)
-{
-	struct stat sb;
-
-	if (stat(fname, &sb))
-		err(1, "%s", fname);
-	tsp[0] = sb.st_atim;
-	tsp[1] = sb.st_mtim;
-}
-
-static void
-usage(const char *myname)
-{
-	fprintf(stderr, "usage: %s [-A [-][[hh]mm]SS] [-achm] [-r file] "
-		"[-t [[CC]YY]MMDDhhmm[.SS]]\n"
-		"       [-d YYYY-MM-DDThh:mm:SS[.frac][tz]] "
-		"file ...\n", myname);
-	exit(1);
+  var usage : String { get { """
+usage: \(programName) [-A [-][[hh]mm]SS] [-achm] [-r file] [-t [[CC]YY]MMDDhhmm[.SS]]
+                      [-d YYYY-MM-DDThh:mm:SS[.frac][tz]] file ...
+"""
+  }
+  }
 }
--- Tests/touch/touchTest.swift
+++ Tests/touch/touchTest.swift
@@ -1,4 +1,152 @@
 // Copyright (c) 1868 Charles Babbage
 // Modernized by Robert "r0ml" Lefkowitz <code@liberally.net> in 2025
 
-import Foundation
+import ShellTesting
+import CMigration
+
+struct touchTest : ShellTest {
+  var cmd = "touch"
+  var suiteBundle = "touchTest"
+
+  @Test("No arguments") func touch_none() async throws {
+    try await run(status: 1, error: /usage: touch/, args: [] )
+  }
+
+  @Test("One argument") func touch_one() async throws {
+    let foo = try tmpfile("foo1")
+    try await run(args: [foo])
+    #expect( FileManager.default.fileExists(atPath: foo.path ) )
+    rm(foo)
+  }
+
+  @Test("Multiple arguments") func touch_multiple() async throws {
+    let foo = try tmpfile("foo")
+    let bar = try tmpfile("bar")
+    let baz = try tmpfile("baz")
+    try await run(args: [foo, bar, baz])
+    print("foo")
+    #expect(FileManager.default.fileExists(atPath: foo.path))
+    #expect(FileManager.default.fileExists(atPath: bar.path))
+    #expect(FileManager.default.fileExists(atPath: baz.path))
+    rm(foo, bar, baz)
+  }
+
+  @Test("Absolute date / time") func absolute() async throws {
+    let foo = try tmpfile("foo2")
+    try await run(args: ["-t", "7001010101", foo], env: ["TZ":"UTC"])
+    let m = try FileMetadata(for: foo.path)
+    #expect(m.lastWrite.timeInterval == 3660)
+    rm(foo)
+
+    try await run(args: ["-t", "7001010101.01", foo], env: ["TZ":"UTC"])
+    let m2 = try FileMetadata(for: foo.path)
+    #expect(m2.lastWrite.timeInterval == 3661)
+    rm(foo)
+
+    try await run(args: ["-t", "196912312359", foo], env: ["TZ":"UTC"])
+    let m3 = try FileMetadata(for: foo.path)
+    #expect(m3.lastWrite.timeInterval == -60)
+    rm(foo)
+
+    try await run(args: ["-t", "196912312359.58", foo], env: ["TZ":"UTC"])
+    let m4 = try FileMetadata(for: foo.path)
+    #expect(m4.lastWrite.timeInterval == -2)
+    rm(foo)
+
+    // the time specification winds up being "-1" -- which registers as an error
+    try await run(status: 1, error: /out of range/, args: ["-t", "196912312359.59", foo], env: ["TZ":"UTC"])
+    rm(foo)
+
+    try await run(args: ["-d1969-12-31T23:59:58", foo], env: ["TZ":"UTC"])
+    let m6 = try FileMetadata(for: foo.path)
+    #expect(m6.lastWrite.timeInterval == -2)
+    rm(foo)
+
+    try await run(args: ["-d1969-12-31 23:59:58", foo], env: ["TZ":"UTC"])
+    let m7 = try FileMetadata(for: foo.path)
+    #expect(m7.lastWrite.timeInterval == -2)
+    rm(foo)
+
+    try await run(args: ["-d1970-01-01T00:59:58", foo], env: ["TZ":"CET"])
+    let m8 = try FileMetadata(for: foo.path)
+    #expect(m8.lastWrite.timeInterval == -2)
+    rm(foo)
+
+    try await run(args: ["-d1970-01-01T00:59:58Z", foo], env: ["TZ":"CET"])
+    let m9 = try FileMetadata(for: foo.path)
+    #expect(m9.lastWrite.timeInterval == 3598)
+    rm(foo)
+
+    try await run(args: ["-d1969-12-31T23:59:59Z", foo], env: ["TZ":"CET"])
+    rm(foo)
+  }
+
+  @Test("Relative date / time") func relative() async throws {
+    let foo = try tmpfile("foo3")
+    try await run(args: ["-t", "202403241234.56", foo], env: ["TZ":"UTC"])
+    let m = try FileMetadata(for: foo.path)
+    #expect(m.lastWrite.timeInterval == 1711283696)
+
+    try await run(args: ["-A", "-36", foo], env: ["TZ":"UTC"])
+    let m2 = try FileMetadata(for: foo.path)
+    #expect(m2.lastWrite.timeInterval == 1711283660)
+
+    try await run(args: ["-A", "-0100", foo], env: ["TZ":"UTC"])
+    let m3 = try FileMetadata(for: foo.path)
+    #expect(m3.lastWrite.timeInterval == 1711283600)
+
+    try await run(args: ["-A", "-010000", foo], env: ["TZ":"UTC"])
+    let m4 = try FileMetadata(for: foo.path)
+    #expect(m4.lastWrite.timeInterval == 1711280000)
+
+    try await run(args: ["-A", "010136", foo], env: ["TZ":"UTC"])
+    let m5 = try FileMetadata(for: foo.path)
+    #expect(m5.lastWrite.timeInterval == 1711283696)
+    rm(foo)
+  }
+
+  @Test("Copy time from another file") func copy() async throws {
+    let foo = try tmpfile("foo4")
+    let bar = try tmpfile("bar4")
+    try await run(args: ["-t", "202403241234.56", foo], env: ["TZ":"UTC"])
+    let m = try FileMetadata(for: foo.path)
+    #expect(m.lastWrite.timeInterval == 1711283696)
+
+    try await run(args: ["-t", "7001010000", bar], env: ["TZ":"UTC"])
+    let m2 = try FileMetadata(for: bar.path)
+    #expect(m2.lastWrite.timeInterval == 0)
+
+    try await run(args: ["-r", foo, bar])
+    let m3 = try FileMetadata(for: bar.path)
+    #expect(m3.lastWrite.timeInterval == 1711283696)
+    rm(foo, bar)
+  }
+
+  @Test("Do not create file") func nocreate() async throws {
+    let foo = try tmpfile("foo5")
+    let bar = try tmpfile("bar5")
+    rm(foo, bar)
+    try await run(args: ["-t", "202403241234.56", foo], env: ["TZ":"UTC"])
+    let m = try FileMetadata(for: foo.path)
+    #expect(m.lastWrite.timeInterval == 1711283696)
+
+    try await run(args: ["-c", "-t", "7001010000", foo, bar], env: ["TZ":"UTC"])
+    let m2 = try FileMetadata(for: foo.path)
+    #expect(m2.lastWrite.timeInterval == 0)
+    #expect(!FileManager.default.fileExists(atPath: bar.path))
+    try await run(args: ["-c", bar])
+    #expect(!FileManager.default.fileExists(atPath: bar.path))
+
+    rm(foo, bar)
+  }
+
+  @Test("Verifying that touch(1)ing an existing file sets its modification time to be later than its creation time") func rdar70075417() async throws {
+    let filename = try tmpfile("XXXX6", "")
+    let c1 = try FileMetadata(for: filename.path).created
+    try await run(args: [filename])
+
+    let m1 = try FileMetadata(for: filename.path).lastWrite
+    #expect(m1.timeInterval > c1.timeInterval)
+
+  }
+}
