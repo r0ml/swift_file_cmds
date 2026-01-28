7be4118fb07b5ba59b4e8faa4459ef2ded85aff6
Robert Lefkowitz <r0ml@mac.com>
2026-01-28 11:57:38 -0500

    so multiple modulemaps in subdirectories works for XCode?

--- Vendors/bzlib_shim.h
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 1868 Charles Babbage
-// Modernized by Robert "r0ml" Lefkowitz <code@liberally.net> in 2026
-
-#pragma once
-#include <bzlib.h>
--- Vendors/curses_shim.h
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 1868 Charles Babbage
-// Modernized by Robert "r0ml" Lefkowitz <code@liberally.net> in 2026
-
-#pragma once
-#include <curses.h>
-#include <term.h>
